320cf45e9bdd6d7cb3c523069098e7b95ff5c161
Nabz786 <nabvali98@gmail.com>
2018-01-23 12:21:34 -0500

    Added game classes

--- src/application/Game/Game.java
+++ src/application/Game/Game.java
@@ -1,12 +1,13 @@
 package application.Game;
 
+import javafx.animation.AnimationTimer;
+
 /********************************************************************** 
  * This class will hold all the relevant material related to the game
  * and game states 
  *
  *********************************************************************/
 
-
 public class Game {
-
+	
 }
--- src/application/Game/Sprite.java
+++ src/application/Game/Sprite.java
@@ -1,5 +1,11 @@
 package application.Game;
 
+/**********************************************************************
+ * This class will represent the basic (Super class) for sprites 
+ * all other sprites will inherit this class 
+ *********************************************************************/
+
+
 public class Sprite {
 
 }
