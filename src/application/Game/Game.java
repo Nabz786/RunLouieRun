364f2369a9d0a11286dde8dfc0946c056ef0153f
Nabz786 <nabvali98@gmail.com>
2018-01-24 10:01:53 -0500

    Minor comment change

--- src/application/Game/Game.java
+++ src/application/Game/Game.java
@@ -1,13 +1,15 @@
 package application.Game;
 
-import javafx.animation.AnimationTimer;
-
 /********************************************************************** 
  * This class will hold all the relevant material related to the game
  * and game states 
  *
  *********************************************************************/
 
+
+
+//TODO
+//Animation timer will probably be used to update frames approx 60 times second. 
 public class Game {
 	
 }
