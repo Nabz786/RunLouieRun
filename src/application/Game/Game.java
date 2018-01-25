fd26a4bfc6fd041c45469edd1dc438b199c8bd89
Nabz786 <nabvali98@gmail.com>
2018-01-25 12:00:41 -0500

    Added animation timer to game class

--- src/application/Game/Game.java
+++ src/application/Game/Game.java
@@ -1,15 +1,33 @@
 package application.Game;
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /********************************************************************** 
  * This class will hold all the relevant material related to the game
  * and game states 
  *
  *********************************************************************/
-
-
-
-//TODO
-//Animation timer will probably be used to update frames approx 60 times second. 
-public class Game {
+public class Game extends Application {
 	
-}
+	
+	public Game() {
+		timer.start();
+	}
+	
+	AnimationTimer timer = new AnimationTimer() {
+		
+		@Override
+		public void handle(long timerTick) {
+			
+		}
+	};
+	
+	
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+	}
+}	
+
