229a67148e59ddc2afdede475d45c04e35089420
Nabz786 <nabvali98@gmail.com>
2018-01-25 12:00:59 -0500

    Re-wrote main menu

--- src/application/Main/Main.java
+++ src/application/Main/Main.java
@@ -1,10 +1,16 @@
 package application.Main;
+import application.Game.Game;
  
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 /**********************************************************************
  * This is the main class, here the program will be launched placing
@@ -13,24 +19,56 @@ import javafx.scene.Scene;
  *********************************************************************/
 public class Main extends Application {
 	
-	/** Int for the with of the program window **/
+	/** Width of the program window **/
 	private static final int WIDTH = 650;
 	
-	/** Int of the height of the program window **/
+	/** height of the program window **/
 	private static final int HEIGHT = 400;
 	
+	/** Stage for the primary stage which will be the main-window **/
+	private Stage mainWindow;
+	
+	/** The first scene that loads upon running the program **/
+	private Scene mainMenuScene;
+	
+	/** Button to start the game**/
+	private Button startButton;
+	
+	
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
+	public void start(Stage primaryStage) throws IOException {
+		//try {
+			mainWindow = primaryStage;
+			startButton = new Button("Start");
+			startButton.setPrefSize(70, 50);
+			
+			VBox startMenuLayout = new VBox();
+			startMenuLayout.setAlignment(Pos.CENTER);
+			startMenuLayout.getChildren().add(startButton); //When adding multiple children use .addAll
+			
+			mainMenuScene = new Scene(startMenuLayout, WIDTH, HEIGHT);
+			mainWindow.setScene(mainMenuScene);
+			mainWindow.setTitle("Run Louie Run");
+			mainWindow.show();
+			mainWindow.setResizable(false);
+			
 			Parent root = FXMLLoader.load(getClass().getResource("MainMenuStyle.fxml"));
-			Scene scene = new Scene(root,WIDTH,HEIGHT);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Run Louie Run");
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
+			startButton.setOnAction(event ->{
+				mainWindow.setScene(new Scene(root, WIDTH, HEIGHT));
+				Game game = new Game();
+			});
+			
+			
+			
+			
+//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+		
+		
+		//create a start button, then within the event listener create the animation timer
+		//thus when start is clicked we create a new timer for all the animations.
 	}
 	
 	public static void main(String[] args) {
