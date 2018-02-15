8ddf634c4f31abee76ab7ae08c28b296f9dd9241
Kehlsoo <lewiskeh@mail.gvsu.edu>
2018-02-15 12:10:45 -0500

    modified game code and menu code

--- src/application/Main/Main.java
+++ src/application/Main/Main.java
@@ -1,16 +1,34 @@
 package application.Main;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
 import application.Game.Game;
 import application.Game.SoundManager;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 /**********************************************************************
@@ -28,7 +46,7 @@ public class Main extends Application {
 	private static final int HEIGHT = 400; // transition to 400
 
 	/** Stage for the primary stage which will be the main-window **/
-	private static Stage mainWindow;
+	public static Stage mainWindow;
 
 	/** The first scene that loads upon running the program **/
 	private Scene mainMenuScene;
@@ -36,22 +54,21 @@ public class Main extends Application {
 	/** Button to start the game **/
 	private Button startButton;
 	
-	/** Manager for all game sounds **/
 	private SoundManager soundManager;
 	
 
 	@Override
 	public final void start(final Stage primaryStage) throws IOException {
 	
-		
-		soundManager = new SoundManager();
-		soundManager.playSound(SoundManager.Sounds.MainMenu);
+		Parent root = FXMLLoader.load(getClass().getResource("MainMenuStyle.fxml"));
+//		soundManager = new SoundManager();
+//		soundManager.playSound(SoundManager.Sounds.MainMenu);
 		
 		
 		mainWindow = primaryStage;
 
 		//I changed from vbox to pane because I found more tutorials on pane, seemed easier to work with :)
-		Pane startMenuLayout = new Pane();
+		//Pane startMenuLayout = new Pane();
 		
 		//Commented out my file locations, you guys do the same for yours so we don't have to keep changing them as we move the files around
 		// for not at least until we find a different way
@@ -60,59 +77,53 @@ public class Main extends Application {
 //		ImageView title = new ImageView(new Image("file:///C:/Users/Kehlsey/workspace/RunLouieRun/src/application/Resources/Images/title.png"));
 //		ImageView startButton = new ImageView(new Image("file:///C:/Users/Kehlsey/workspace/RunLouieRun/src/application/Resources/Images/start_text.png"));
 //		ImageView exitButton = new ImageView(new Image("file:///C:/Users/Kehlsey/workspace/RunLouieRun/src/application/Resources/Images/exit_text.png"));
-//		ImageView background = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/background.png"));
-//		ImageView title = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/title.png"));
-//		ImageView startButton = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/start_text.png"));
-//		ImageView exitButton = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/exit_text.png"));
-		ImageView background = new ImageView(new Image("file:///C:/Users/Andy/Documents/GitHub/RunLouieRun/src/application/Resources/Images/background.png"));
-		ImageView title = new ImageView(new Image("file:///C:/Users/Andy/Documents/GitHub/RunLouieRun/src/application/Resources/Images/title.png"));
-		ImageView startButton = new ImageView(new Image("file:///C:/Users/Andy/Documents/GitHub/RunLouieRun/src/application/Resources/Images/start_text.png"));
-		ImageView exitButton = new ImageView(new Image("file:///C:/Users/Andy/Documents/GitHub/RunLouieRun/src/application/Resources/Images/exit_text.png"));
-		
-		startButton.setLayoutX(400);
-		startButton.setLayoutY(150);
-		
-		exitButton.setLayoutX(400);
-		exitButton.setLayoutY(200);
-		
-		background.setLayoutX(0);
-		background.setLayoutY(0);
-		
-		title.setLayoutX(150);
-		title.setLayoutY(0);
-				
-		//adding a listener to the image to act as a button
-		startButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-			
-			//code to start game goes here
-			//when the start button is clicked we create a new instance of game and load it to the current stage
-						Game game = null;
-						try {
-								game = new Game();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						mainWindow.setScene(game.getGameScene());
-						soundManager.stopSound();
-						soundManager.playSound(SoundManager.Sounds.Running);
-						
-						
-
-			//this tells the handler that the event is over
-	         event.consume();	
-		});
-
-		//closes the window
-		exitButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-			primaryStage.close();				
-		});
-		
-		//adding all of the menu components
-		startMenuLayout.getChildren().addAll(background,title,exitButton,startButton);
+////		ImageView background = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/background.png"));
+////		ImageView title = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/title.png"));
+////		ImageView startButton = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/start_text.png"));
+////		ImageView exitButton = new ImageView(new Image("file:///C:/Users/Nabeel/eclipse-workspace/RunLouieRun/src/application/Resources/Images/exit_text.png"));
+////		
+//		startButton.setLayoutX(400);
+//		startButton.setLayoutY(150);
+//		
+//		exitButton.setLayoutX(400);
+//		exitButton.setLayoutY(200);
+//		
+//		background.setLayoutX(0);
+//		background.setLayoutY(0);
+//		
+//		title.setLayoutX(150);
+//		title.setLayoutY(0);
+//				
+//		//adding a listener to the image to act as a button
+//		startButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+//			
+//			//code to start game goes here
+//			//when the start button is clicked we create a new instance of game and load it to the current stage
+//						Game game = null;
+//						try {
+//								game = new Game();
+//							} catch (IOException e) {
+//								e.printStackTrace();
+//							}
+//						mainWindow.setScene(game.getGameScene());
+//						soundManager.stopSound();
+//						
+//
+//			//this tells the handler that the event is over
+//	         event.consume();	
+//		});
+//
+//		//closes the window
+//		exitButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+//			primaryStage.close();				
+//		});
+//		
+//		//adding all of the menu components
+//		startMenuLayout.getChildren().addAll(background,title,exitButton,startButton);
 		
 
 		//Main window options
-		mainMenuScene = new Scene(startMenuLayout, WIDTH, HEIGHT);
+		mainMenuScene = new Scene(root, WIDTH, HEIGHT);
 		mainWindow.setScene(mainMenuScene);
 		mainWindow.setTitle("Run Louie Run");
 		mainWindow.show();
@@ -123,6 +134,11 @@ public class Main extends Application {
 			
 	// returns the current stage
 	public static Stage getCurrentStage() {
+		return mainWindow;
+	}
+	
+	public static Stage setScene(Scene scene){
+		mainWindow.setScene(scene);
 		return mainWindow;
 	}
 
--- src/application/Main/MainMenuController.java
+++ src/application/Main/MainMenuController.java
@@ -1,8 +1,15 @@
 package application.Main;
 
+import java.io.IOException;
+
+import application.Game.Game;
+import application.Game.SoundManager;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**********************************************************************
  * This is the fx:controller class which links the scene builder
@@ -11,6 +18,7 @@ import javafx.scene.control.Label;
  *********************************************************************/
 
 public class MainMenuController {
+
 			
 	@FXML
 	private Button startButton;
@@ -18,13 +26,42 @@ public class MainMenuController {
 	
 	@FXML
 	private Label testLabel;
+	private Stage mainWindow;
+	private SoundManager soundManager;
+	private Scene mainMenuScene;
+	private Main main;
+	
+	/** Width of the program window **/
+	private static final int WIDTH = 600; // transition to 600
+
+	/** height of the program window **/
+	private static final int HEIGHT = 400; // transition to 400
 
 	//This method is called after all @FXML annotated members have been injected
 	@FXML
 	private void initialize() {
+		soundManager = new SoundManager();
+		soundManager.playSound(SoundManager.Sounds.MainMenu);
+		
 		startButton.setOnAction(e -> {
-			System.out.println("test");
+			
+			//System.out.println("test");
+			Game game = null;
+			try {
+					game = new Game();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			//mainWindow.setScene(game.getGameScene());
+			Main.setScene(game.getGameScene());
+			soundManager.stopSound();
+			
 		});
 
 	}
+	
+	
+	
+	
+	
 }
