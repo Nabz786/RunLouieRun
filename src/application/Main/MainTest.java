a3464414e56479799ef8a97ffce136b43baf4c6a
afreiman1 <freimana@mail.gvsu.edu>
2018-02-15 11:28:35 -0500

    Added JUnit Test Class

---
+++ src/application/Main/MainTest.java
@@ -0,0 +1,15 @@
+package application.Main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	public void Test2equals() {
+		assertTrue(2==4/2);
+	}
+	
+	
+}
