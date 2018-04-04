34401d23dde0f42c1cfb418c3b4249000ab14769
Nabz786 <nabvali98@gmail.com>
2018-04-04 10:14:41 -0400

    Started work on an assethandler to better manage all assets

---
+++ src/application/Game/AssetLoader.java
@@ -0,0 +1,23 @@
+package application.Game;
+
+/**
+ * Holds and loads all game assets.
+ * @author Nabeel
+ */
+public class AssetLoader {
+	
+	/**
+	 * Default constructor, calls loadAllAssets().
+	 */
+	public AssetLoader() {
+		loadAllAssets();
+	}
+	
+	/**
+	 * Loads all assets for the entire game.
+	 */
+	private void loadAllAssets() {
+		
+	}
+
+}
