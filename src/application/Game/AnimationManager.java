21ba07e37d605e63859b937867c66a9ee8e9c721
Nabz786 <nabvali98@gmail.com>
2018-04-04 10:14:11 -0400

    Started work on an animationManager to hold all animations

---
+++ src/application/Game/AnimationManager.java
@@ -0,0 +1,24 @@
+package application.Game;
+
+/**
+ * Holds and creates all animations within the game.
+ * @author Nabeel
+ */
+public class AnimationManager {
+
+	/**
+	 * Default constructor.
+	 */
+	public AnimationManager() {
+		
+	}
+	
+	
+	/**
+	 * Creates the starting (countdown animation).
+	 */
+	private void displayStartingAnim() {
+		
+	}
+	
+}
