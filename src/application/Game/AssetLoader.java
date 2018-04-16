9f25fb23e07a4331de449f805334456c6e3d3ef3
Nabz786 <nabvali98@gmail.com>
2018-04-15 23:30:18 -0400

    Changed bg width back to 600

--- src/application/Game/AssetLoader.java
+++ src/application/Game/AssetLoader.java
@@ -7,7 +7,7 @@ package application.Game;
 public class AssetLoader {
 	
 	/** Width of program window.**/
-	private static final int WIDTH = 700;
+	private static final int WIDTH = 600;
 	
 	/** Height of program window.**/
 	private static final int HEIGHT = 400;
