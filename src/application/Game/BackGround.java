83f0ca987b1e63932944d42b4343eb4a5e589958
Nabz786 <nabvali98@gmail.com>
2018-04-15 23:30:43 -0400

    Created new sprite class for the moving background

---
+++ src/application/Game/BackGround.java
@@ -0,0 +1,87 @@
+package application.Game;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class BackGround {
+
+	/** The image for the background .**/
+	private Image backGround;
+	
+	/** Y-axis position of the sprite.**/
+	private double positionY;
+	
+	/** X-axis position of the sprite.**/
+	private double positionX;
+	
+	/**
+	 * Constructor creates a new background image at specified
+	 * x,y position.
+	 * @param positionX - X coord to place background
+	 * @param positionY - Y coord to place background
+	 */
+	public BackGround(final double positionX, final double positionY) {
+		setPosition(positionX, positionY);
+		setBackGround(new Image("file:resources/Images/BGF.png"));
+	}
+	
+	/**
+	 * Called from Game, renders background's position on the canvas.
+	 * @param gc Graphics Context
+	 * @param deltaDifference Difference in time between current/last frame
+	 */
+	public void render(final GraphicsContext gc) {
+		 gc.drawImage(backGround,
+	        		getPositionX(), getPositionY(),
+	        		2200, 400);
+    }
+	
+	/**
+	 * Decrements the background's x value, moving it right.
+	 */
+	public void moveRight() {
+		setPosition(getPositionX() - 4, getPositionY());
+	}
+
+	/**
+	 * Returns the background image.
+	 * @return - Image of background
+	 */
+	public Image getBackGround() {
+		return backGround;
+	}
+
+	/**
+	 * Sets the background sprite's image.
+	 * @param backGround - image to set
+	 */
+	public void setBackGround(Image backGround) {
+		this.backGround = backGround;
+	}
+	
+	/**
+	 * Sets the position of a sprite manually.
+	 * @param x - x coordinate on screen 
+	 * @param y - y coordinate on screen
+	 */
+	public void setPosition(final double x, final double y) {
+        positionX = x;
+        positionY = y;
+    }
+	
+	/**
+	 * Returns the current sprites x coordinate.
+	 * @return Current sprites x coordinate
+	 */
+	public double getPositionX() {
+		return positionX;
+	}
+	
+	/**
+	 * Returns the sprites y coordinate.
+	 * @return Current sprites y coordinate
+	 */
+	public double getPositionY() {
+		return positionY;
+	}
+}
